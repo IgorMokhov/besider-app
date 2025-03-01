560212e6eda6a0dfdd33ef911fb961ccdcc720aa
Igor Mokhov <igormokhovid@gmail.com>
2025-03-01 17:56:00 +0700

    feat: added Sidebar

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,32 @@
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import {
+  closeSidebar,
+  selectSidebar,
+} from '../../redux/slices/sidebar/sidebarSlice';
+import closeIcon from '../../assets/icons/close-icon.svg';
+import { SidebarLink } from '../SidebarLink/SidebarLink';
+import { links } from '../../data/links';
+import styles from './Sidebar.module.scss';
+
+export const Sidebar = () => {
+  const isOpen = useAppSelector(selectSidebar);
+  const dispatch = useAppDispatch();
+
+  return (
+    <aside className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
+      <button
+        className={styles.sidebar_closeBtn}
+        onClick={() => dispatch(closeSidebar())}
+      >
+        <img src={closeIcon} alt="close" />
+      </button>
+      <nav>
+        <ul className={styles.sidebar_list}>
+          {links.map(({ name, path }) => (
+            <SidebarLink name={name} path={path} key={name} />
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
