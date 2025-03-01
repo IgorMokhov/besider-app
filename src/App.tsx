fe4009a20727efccda5de1c82462272f99f4f319
Igor Mokhov <igormokhovid@gmail.com>
2025-03-01 17:56:36 +0700

    feat: added Sidebar in the app and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Route, Routes } from 'react-router';
-import styles from './App.module.scss';
 import { HomePage } from './pages/HomePage';
+import { Sidebar } from './components/Sidebar/Sidebar';
+import styles from './App.module.scss';
 
 export const App = () => {
   return (
     <div className={styles.app}>
       <Header />
+      <Sidebar />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
