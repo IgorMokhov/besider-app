3f6f7f31d012006a37f69559dca47be3b25cb0ff
Igor Mokhov <igormokhovid@gmail.com>
2025-03-01 17:57:24 +0700

    feat: added button for opening Sidebar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import styles from './Header.module.scss';
 import menuIcon from '../../assets/icons/menu-icon.svg';
+import { useAppDispatch } from '../../redux/hooks';
+import { openSidebar } from '../../redux/slices/sidebar/sidebarSlice';
 
 export const Header = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <header className={styles.header}>
       <h1 className={styles.header_title}>BESIDER</h1>
-      <button className={styles.header_button} aria-label="Open menu">
+      <button
+        className={styles.header_button}
+        onClick={() => dispatch(openSidebar())}
+        aria-label="Open menu"
+      >
         <img className={styles.header_button_icon} src={menuIcon} alt="menu" />
       </button>
     </header>
