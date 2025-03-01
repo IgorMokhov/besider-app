311aa9a81703ece69d2376f0cd3c1cd80de16549
Igor Mokhov <igormokhovid@gmail.com>
2025-03-01 15:31:56 +0700

    feat: added News

---
+++ src/components/News/News.tsx
@@ -0,0 +1,10 @@
+import { Container } from '../../UI/Container/Container';
+import styles from './News.module.scss';
+
+export const News = () => {
+  return (
+    <main className={styles.news}>
+      <Container>News Content</Container>
+    </main>
+  );
+};
