3c92faaee41e05f0915358a9819742302dcfc006
Igor Mokhov <igormokhovid@gmail.com>
2025-03-02 23:06:10 +0700

    feat: added types for news

---
+++ src/types/news.ts
@@ -0,0 +1,26 @@
+export interface IMultimedia {
+  subType: 'thumbLarge' | 'thumbnail';
+  url: string;
+}
+
+export interface IArticle {
+  abstract: string;
+  web_url: string;
+  pub_date: string;
+  source: string;
+  multimedia: IMultimedia[];
+}
+
+export interface IGetNewsResponse {
+  response: {
+    docs: IArticle[];
+  };
+}
+
+export interface INewsItem {
+  title: string;
+  url: string;
+  date: string;
+  source: string;
+  imageUrl: string;
+}
